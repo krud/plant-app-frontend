882105e4c139e0d7373a02976d2f74e458588e9e
KellyRudnicki <kelly.rudnicki@comcast.net>
2020-02-24 11:04:50 -0700

    initial commit, testing by changing text on ContentView

--- PlantApp/ContentView.swift
+++ PlantApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
  
     var body: some View {
         TabView(selection: $selection){
-            Text("First View")
+            Text("Home")
                 .font(.title)
                 .tabItem {
                     VStack {
@@ -22,7 +22,7 @@ struct ContentView: View {
                     }
                 }
                 .tag(0)
-            Text("Second View")
+            Text("2nd View")
                 .font(.title)
                 .tabItem {
                     VStack {
